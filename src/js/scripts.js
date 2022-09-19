c664d078c1c68388cb037cc1057d1fb29849b769
smolanovsergey <smolanovsergey@gmail.com>
2022-09-19 19:24:06 +0300

    мобайл

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -10,7 +10,26 @@ $(document).ready(function () {
     });
 
 
-    AOS.init();
+    AOS.init({
+        // Global settings:
+        disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+        startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+        initClassName: 'aos-init', // class applied after initialization
+        animatedClassName: 'aos-animate', // class applied on animation
+        useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+        disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+        debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+        throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+        // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+        offset: 120, // offset (in px) from the original trigger point
+        delay: 0, // values from 0 to 3000, with step 50ms
+        duration: 800, // values from 0 to 3000, with step 50ms
+        easing: 'ease', // default easing for AOS animations
+        once: true, // whether animation should happen only once - while scrolling down
+        mirror: false, // whether elements should animate out while scrolling past them
+        anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+    });
 
 
     if ($('.faq').length) {
@@ -20,10 +39,10 @@ $(document).ready(function () {
         });
     }
 
-    $(".header__burger").click(function () {
+    $(".header__mobile-burger").click(function () {
         $(this).toggleClass('active');
         $('body').toggleClass('locked');
-        $('.header__mobile').fadeToggle(300);
+        $('.header__container').fadeToggle().css('display', 'flex');
     });
     if ($('.customers').length) {
         var customers = new Swiper(".customers__list", {
@@ -33,6 +52,27 @@ $(document).ready(function () {
                 el: ".customers__pagination",
                 clickable: true,
             },
+            breakpoints: {
+                // when window width is >= 320px
+                320: {
+                    slidesPerView: 1,
+                    spaceBetween: 20
+                },
+                // when window width is >= 480px
+                480: {
+                    slidesPerView: 1,
+                    spaceBetween: 30
+                },
+                // when window width is >= 640px
+                640: {
+                    slidesPerView: 2,
+                    spaceBetween: 40
+                },
+                960: {
+                    slidesPerView: 3,
+                    spaceBetween: 40
+                }
+            }
         });
     }
 });
